03b5c6075b6edca31d8add549621f7b7a3ae61ec
sabritou <k.sabritou@gmail.com>
2024-03-25 19:54:17 +0100

    V.1.03

--- src/composant/contact/contact.js
+++ src/composant/contact/contact.js
@@ -1,15 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./contact.css"
 
 
 function contact() {
   return (
-    <div>
-    <audio controls>
-    <source src="/src/luigis_mansion.mp3" type="audio/mpeg" />
-    </audio>
-  </div>
+<>
 
+
+<div class="audioplayer">
+<audio controls>
+    <source src="https://s17.aconvert.com/convert/p3r68-cdx67/oavel-q4qe6.mp3" type="audio/mpeg" />
+  </audio>
+</div>
+
+
+
+<center>
+<ol>
+<li><a class="arrow" href=""><i class="fas fa-arrow-alt-right" />Discord</a></li>
+<li><a class="arrow"><i class="fas fa-arrow-alt-right"></i>Github</a></li>
+<li><a class="arrow"><i class="fas fa-arrow-alt-right"></i>Linkedin</a></li>
+<li><a class="arrow"><i class="fas fa-arrow-alt-right"></i>...</a></li>
+<li><a class="arrow"><i class="fas fa-arrow-alt-right"></i>...</a></li>
+</ol>
+</center>
+</>
     )
 }
 
--- src/composant/propos/propos.js
+++ src/composant/propos/propos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./propos.css"
 
 function propos() {
     return (
@@ -6,10 +7,49 @@ function propos() {
         <>
         
         
-        <h1>autre test</h1>
         
-        <h2>ddsddsferfgerf</h2>
-        
+<html>
+<head>
+    
+</head>
+<body>
+
+
+    
+    <div class="container">
+        <div class="movie-info">
+            <h2>À Propos</h2>
+            <img src="/src/img/ada.png" alt='Ada'></img>
+            <h3>Super Mario Bros</h3>
+            <p>Dos hermanos plomeros, Mario y Luigi, caen por las alcantarillas y llegan a un mundo subterráneo mágico en el que deben enfrentarse al malvado Bowser para rescatar a la princesa Peach, quien ha sido forzada a aceptar casarse con él.</p>
+            <div class="rating">★★★★☆ 4/5</div>
+            <div class="director"><span>Director:</span> Aaron Horvath</div>
+            <div class="cast"><span>Reparto:</span> Anya Taylor-Joy, Jack Black,Chris Pratt,  Chris Pratt</div>
+            <div class="genre"><span>Género:</span> Acción, Aventura, Comedia</div>
+            <div class="release-date"><span>Fecha de lanzamiento:</span>5 de abril de 2023</div>
+            <div class="recommendation">93%</div>
+        </div>
+    </div>
+
+
+
+    <div class="container2">
+        <div class="movie-info">
+            <h2>À Propos</h2>
+            <img src="..."></img>
+            <h3>Super Mario Bros</h3>
+            <p>Dos hermanos plomeros, Mario y Luigi, caen por las alcantarillas y llegan a un mundo subterráneo mágico en el que deben enfrentarse al malvado Bowser para rescatar a la princesa Peach, quien ha sido forzada a aceptar casarse con él.</p>
+            <div class="rating">★★★★☆ 4/5</div>
+            <div class="director"><span>Director:</span> Aaron Horvath</div>
+            <div class="cast"><span>Reparto:</span> Anya Taylor-Joy, Jack Black,Chris Pratt,  Chris Pratt</div>
+            <div class="genre"><span>Género:</span> Acción, Aventura, Comedia</div>
+            <div class="release-date"><span>Fecha de lanzamiento:</span>5 de abril de 2023</div>
+            <div class="recommendation">93%</div>
+        </div>
+    </div>
+</body>
+</html>
+
         
         </>
 
